6c18aa1d696cb5d7bb4ad5937a69fa8392676bd4
razy <razy.dev@spiderkim.com>
2022-12-20 17:57:28 +0900

    working ...

---
+++ src/linear_regression/lr_linear.py
@@ -0,0 +1,86 @@
+# tag::preset[]
+import matplotlib
+import torch
+
+torch.manual_seed(1)  # 난수 고정
+torch.set_printoptions(threshold=10, linewidth=128)  # for printing
+matplotlib.use('MacOSX')  # 'module://backend_interagg', 'GTK3Agg', 'GTK3Cairo' ...
+# end::preset[]
+
+# tag::dataset[]
+BATCH_SIZE = 100
+
+
+def build_dataset(size: int, batch_size: int = None, train_rate: float = 0.7) -> tuple:
+    x = torch.linspace(0, 10, steps=size, dtype=float).float()
+    y = 10 * x + torch.randint(-2, 2, (size,))
+    train_len = round(size * train_rate)
+    x_train = x[:train_len].unsqueeze(dim=1)
+    y_train = y[:train_len].unsqueeze(dim=1)
+    x_test = x[train_len:].unsqueeze(dim=1)
+    y_test = y[train_len:].unsqueeze(dim=1)
+    return x_train, y_train, x_test, y_test
+
+
+x_train, y_train, x_test, y_test = build_dataset(BATCH_SIZE)
+
+# Plot
+# plt.scatter(x_train, y_train)
+# plt.show()
+# end::dataset[]
+
+# tag::hypothesis[]
+model = torch.nn.Linear(in_features=1, out_features=1, bias=True)
+# end::hypothesis[]
+
+# tag::cost[]
+criterion = torch.nn.MSELoss(reduction='mean')
+
+# end::cost[]
+
+
+# tag::optimizer[]
+
+# end::optimizer[]
+
+# tag::training[]
+# tag::parameters[]
+# end::parameters[]
+
+print("\nTraining the model")
+costs = []
+lr = 0.01
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+for e in range(1000):
+    y_pred = model(x_train)
+    cost = criterion(y_pred, y_train)
+    costs.append(cost)
+
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    if not e % 100:
+        print(f"{e:4} : cost = {costs[-1]:>4.8} H(x) = {model.weight.item():>4.5}x + {model.bias.item():>4.5}")
+        # plt.plot(x, hypothesis(x), '--y', label=f"epoch {e}")
+# end::training[]
+
+# tag::test[]
+print("\nValidate the model")
+model.eval()
+with torch.no_grad():
+    for i in range(len(x_test)):
+        predication = model(x_test[i])
+        print(f"input = {x_test[i].item():.5} | output = {predication.item():.5} | real = {y_test[i].item():.5} | loss = {(100 * (predication - y_test[i]) / y_test[i]).item():.3}%")
+# end::test[]
+
+# plt.subplot(211)
+# plt.plot(x, y, 'oc')
+# plt.plot(x, hypothesis(x), 'r')
+# plt.xlabel('x axis')
+# plt.ylabel('y axis')
+#
+# plt.subplot(212)
+# plt.plot(costs)
+# plt.xlabel('epoch')
+# plt.ylabel('cost')
---
+++ src/logistic_regression/lg_numpy.py
@@ -0,0 +1,118 @@
+# tag::preset[]
+import matplotlib
+import numpy as np
+from numpy import ndarray
+
+np.random.seed(1)  # 난수 고정
+matplotlib.use('MacOSX')  # 'module://backend_interagg', 'GTK3Agg', 'GTK3Cairo' ...
+# end::preset[]
+
+# tag::dataset[]
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float)
+
+
+def and_dataset() -> tuple:
+    x = X
+    y = np.array([0, 0, 0, 1], dtype=np.float)
+    return x, y, x, y
+
+
+def or_dataset() -> tuple:
+    x = X
+    y = np.array([0, 1, 1, 1], dtype=np.float)
+    return x, y, x, y
+
+
+def xor_dataset() -> tuple:
+    x = X
+    y = np.array([0, 1, 1, 0], dtype=np.float)
+    return x, y, x, y
+
+
+# Plot
+# plt.scatter(x_train, y_train)
+# plt.show()
+# end::dataset[]
+
+# tag::hypothesis[]
+def sigmoid(z):
+    return 1. / (1 + np.exp(-z))
+
+
+def hypothesis(x1, x2):
+    return sigmoid(w1 * x1 + w2 * x2 + b)
+
+
+# end::hypothesis[]
+
+# tag::cost[]
+def loss(prediction: ndarray, target: ndarray):
+    case_one = -target * np.log(prediction)
+    case_zero = -(1 - target) * np.log(prediction)
+    return case_one + case_zero
+
+
+def criterion(prediction: ndarray, target: ndarray):
+    return np.sum(loss(prediction, target)) / len(prediction)
+
+
+# end::cost[]
+
+# tag::dw[]
+
+
+def w_grad(prediction: ndarray, target: ndarray, x: ndarray):
+    return np.sum(x * (1target - input))
+
+
+# end::dw[]
+
+# tag::db[]
+def b_grad(input: ndarray, target: ndarray):
+    return (-2 / len(target)) * np.sum(target - input)
+
+
+# end::db[]
+
+# tag::optimizer[]
+
+# end::optimizer[]
+
+# tag::training[]
+# tag::parameters[]
+w1 = 1
+w2 = 1
+b = 0
+
+# end::parameters[]
+print("\nTraining the model")
+costs = []
+lr = 0.01
+for e in range(1000):
+    y_pred = hypothesis(x_train)
+    costs.append(criterion(y_pred, y_train))
+    w = w - lr * w_grad(y_pred, y_train, x_train)
+    b = b - lr * b_grad(y_pred, y_train)
+
+    if not e % 100:
+        print(f"{e:4} : cost = {costs[-1]:>4.8} H(x) = {w:>4.5}x + {b:>4.5}")
+        # plt.plot(x, hypothesis(x), '--y', label=f"epoch {e}")
+# end::training[]
+
+# tag::test[]
+print("\nValidate the model")
+for i in range(len(x_test)):
+    predication = hypothesis(x_test[i])
+    print(f"input = {x_test[i]:.5} | output = {predication:.5} | real = {y_test[i]:.5} | loss = {100 * (predication - y_test[i]) / y_test[i]:.3}%")
+# end::test[]
+
+# plt.subplot(211)
+# plt.plot(x, y, 'oc')
+# plt.plot(x, hypothesis(x), 'r')
+# plt.xlabel('x axis')
+# plt.ylabel('y axis')
+#
+# plt.subplot(212)
+# plt.plot(costs)
+# plt.xlabel('epoch')
+# plt.ylabel('cost')
