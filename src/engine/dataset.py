e4d5114a5b3e7b8dddaaf2e4167e6a6cc802ed05
razy <razy.dev@spiderkim.com>
2022-12-27 18:37:11 +0900

    working ...

--- src/engine/dataset.py
+++ src/engine/dataset.py
@@ -7,7 +7,7 @@ torch.manual_seed(1)  # 난수 고정
 torch.set_printoptions(threshold=10, linewidth=128)  # for printing
 
 
-def _load(ticker: str = 'AMZN', start: str = '2013-01-01', end: str = '2021-12-31', size: int = None) -> DataFrame:
+def load(ticker: str = 'AMZN', start: str = '2013-01-01', end: str = '2021-12-31', size: int = None) -> DataFrame:
     try:
         return yf.download(ticker, start=start, end=end, progress=False)[['Adj Close', 'Open', 'High', 'Low', "Close", "Volume"]][:size]
     except Exception as e:
@@ -21,14 +21,29 @@ def _build_dataset(data: DataFrame, window_size: int, output_size: int) -> tuple
     for i in range(window_size, len(data) - output_size + 1):
         x.append(data[i - window_size:i])
         y.append(data[i:i + output_size])
-    return torch.FloatTensor(np.array(x)), torch.FloatTensor(np.array(y))
+    return torch.FloatTensor(np.array(x)), torch.FloatTensor(np.array(y)).squeeze(dim=1)
 
 
 def dataset(data: DataFrame, window_size: int, output_size: int, features: int = 1, split: int = 70) -> tuple:
+    from sklearn.preprocessing import MinMaxScaler
     data = data.iloc[:, :features].values.round(2)
     total = len(data)
     train_size = round(total * split / 100)
-    return _build_dataset(data[:train_size], window_size, output_size) + _build_dataset(data[train_size:], window_size, output_size)
+    scaler = MinMaxScaler()
+    ds = scaler.fit_transform(data)
+    return *(_build_dataset(ds[:train_size], window_size, output_size) + _build_dataset(ds[train_size:], window_size, output_size)), scaler
 
+
+# for test
+if __name__ == "__main__":
+    from sklearn.preprocessing import MinMaxScaler
+
+    d = torch.rand(5, 2) * 10
+    print(d)
+    s = MinMaxScaler()
+    ds = s.fit_transform(d)
+    print(ds)
+    di = s.inverse_transform(ds)
+    print(di)
 # x_train, y_train, x_test, y_test = dataset(_load(size=50), 5, 2)
 # print(x_train, y_train, x_test, y_test)
--- src/engine/simple.py
+++ src/engine/simple.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch import Tensor, optim
 
-from engine.dataset import dataset, _load
+from engine.dataset import dataset, load
 
 torch.manual_seed(1)  # 난수 고정
 torch.set_printoptions(threshold=10, linewidth=128)  # for printing
@@ -11,30 +11,52 @@ time_steps = 5
 in_features = 1
 out_features = 1
 
-x_train, y_train, x_test, y_test = dataset(_load(size=100), window_size=time_steps, features=in_features, output_size=out_features)
+x_train, y_train, x_test, y_test, scaler = dataset(load(size=100), window_size=time_steps, features=in_features, output_size=out_features)
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
 
 # print(x_train, y_train, x_test, y_test)
 
-class Model(nn.Module):
+class NeuralNetwork(nn.Module):
+    num_layers: int = 1
+    bias: bool = True
+    dropout: float = 0.
+    bidirectional: bool = False
 
-    def __init__(self, type: str, input_size, hidden_size):
-        super(Model, self).__init__()
-        self.layer = self.load_model(type, input_size, hidden_size)
+    # x = (k, n) = (time_steps, input_features) => z =
+    def __init__(self, type: str, input_size: int, hidden_size: int, batch_first: bool = False):
+        super(NeuralNetwork, self).__init__()
+        self.layer = self.load_model(type, input_size, hidden_size, batch_first=batch_first)
+        # self.af = nn.Linear(hidden_size, out_features)
 
     def forward(self, x):
         outputs, _status = self.layer(x)
-        return _status[-1:, -1:]
+        y = outputs[:, -1]
+        z = y[:, -1].unsqueeze(dim=1)
+        return z
 
-    def load_model(self, name, input_size, hidden_size):
-        return getattr(nn, name)(input_size, hidden_size)
+    def load_model(self, name, input_size, hidden_size, batch_first):
+        return getattr(nn, name)(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=self.num_layers,
+            bias=self.bias,
+            batch_first=batch_first,
+            dropout=self.dropout,
+            bidirectional=self.bidirectional
+        )
 
 
-learning_rate = 0.01
+z = NeuralNetwork('RNN', in_features, time_steps)(torch.Tensor())
+print(z.shape, z)
+
+learning_rate = 0.001
 
 
 def run(type: str, x_train: Tensor, y_train: Tensor, x_test: Tensor, y_test: Tensor):
-    model = Model(type, in_features, time_steps)
+    import matplotlib.pyplot as plt
+
+    model = NeuralNetwork(type, in_features, time_steps)
     criterion = nn.MSELoss()  # nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), learning_rate)
 
@@ -42,18 +64,33 @@ def run(type: str, x_train: Tensor, y_train: Tensor, x_test: Tensor, y_test: Ten
     print(f"{type} Model")
 
     # Training
-    for e in range(10):
-        for i, x in enumerate(x_train):
-            y_pred = model(x)
-            cost = criterion(y_pred, y_train[i])
-            optimizer.zero_grad()
-            cost.backward()
-            optimizer.step()
-        print(f"{e:>5} : {y_pred.item()} {y_train[i]} {cost}")
+    least = 0
+    costs = []
+    for e in range(100):
+        # for i, x in enumerate(x_train):
+        y_pred = model(x_train)
+        cost: Tensor = criterion(y_pred, y_train)
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+        costs.append(cost.item())
+        print(f"{e:>5} : {cost}")
+    # plt.plot(costs)
 
     # Validate
-    pass
+    outputs = []
+    with torch.no_grad():
+        for i, x in enumerate(x_test):
+            y_pred: Tensor = model(x.unsqueeze(dim=0))
+            outputs.append(y_pred.squeeze().item())
+            print(f"{y_pred.item()} : {y_test[i]}")
     print('\n\n')
+    plt.plot(y_test.squeeze(), "green")
+    plt.plot(outputs, "red")
+    plt.show()
 
 
-run('RNN', x_train, y_train, x_test, y_test)
+if __name__ == "__main__":
+    # run('RNN', x_train, y_train, x_test, y_test)
+    # run('LSTM', x_train, y_train, x_test, y_test)
+    pass
